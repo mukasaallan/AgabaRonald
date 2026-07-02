f54cbe736431b66d5505c8aedbc51f060a06d0b4
mukasaallan <mukasaallan67@gmail.com>
2026-07-03 01:36:03 +0300

    image

--- ronard-tech/components/Header.js
+++ ronard-tech/components/Header.js
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="site-header">
       <div className="container">
-        <h1 className="brand"><Link href="/">Ronard Tech</Link></h1>
+        <h1 className="brand"><Link href="/"><Image src="/image.jpg" alt="Ronard Tech Logo" width={40} height={40} className="logo" /> Ronard Tech</Link></h1>
         <nav className="nav">
           <Link href="/#services">Services</Link>
           <Link href="/projects">Projects</Link>
